3c1254366e89a2468b84509cf51c4c3d971ca91b
streyyj <116295283+streyyj@users.noreply.github.com>
2025-05-05 21:05:01 +0300

    Add files via upload

--- Python_3.py
+++ Python_3.py
@@ -1,22 +1,17 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import SendMessageRequest
 
-# Замени на свои данные
-api_id = 1234567  # Твой API ID
-api_hash = 'ваш_api_hash'  # Твой API Hash
+# Твои данные
+api_id = 1234567  # замени на свой api_id
+api_hash = 'ваш_api_hash'  # замени на свой api_hash
 bot_username = '@F_CardBot'
 
-# Создаем клиент
-client = TelegramClient('session_name', api_id, api_hash)
-
-async def main():
-    # Отправляем сообщение боту
-    await client(SendMessageRequest(
-        peer=bot_username,
-        message='Меню'
-    ))
-    print("Сообщение 'Меню' отправлено боту!")
-
-# Запускаем клиент
-with client:
-    client.loop.run_until_complete(main())
+async def send_menu():
+    async with TelegramClient('session_name', api_id, api_hash) as client:
+        await client.start()
+        print("Клиент запущен")
+        await client(SendMessageRequest(
+            peer=bot_username,
+            message='Меню'
+        ))
+        print("Сообщение 'Меню' отправлено!")
